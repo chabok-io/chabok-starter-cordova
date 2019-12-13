723de0466832806721bc92c55587f3ef9a0a16ed
Farbod <farbod.ps@gmail.com>
2019-12-13 18:31:15 +0330

    fix sample project javascript file to use new plugin

--- chabok-starter-cordova/www/js/index.js
+++ chabok-starter-cordova/www/js/index.js
@@ -16,9 +16,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var chabok;
+
 var app = {
     // Application Constructor
     initialize: function() {
+        console.log('=====================');
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -29,64 +33,82 @@ var app = {
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
 
-        const chabok = new ChabokPush();
+        chabok = new ChabokPush();
 
         console.log('------------------');
 
         console.log('cordova.plugins = ', JSON.stringify(chabok));
 
-        let options = {
-            appId: 'adp-nms-push/845225163503',
-            apiKey: 'e2100f0d7e071c7450f04e530bda746da2fc493b',
-            username: 'adp',
-            password: 'test',
-            devMode: true
-        }
-        console.log('----- options = ', options);
-
-        chabok.init(options, function (s) {
-            console.log('Initialize successfully. s = ' + s);
-        }, function (err) {
-            console.error('Could not initialize = ' + err);
-        });
-
-        chabok.getUserId((userId)=>{
-            console.log('-- Call register method');
-            chabok.register(userId, function (s){
-                console.log('Registered user successfully, s = ' + s);
-
-                chabok.track('registerAgain', {id: 123});
-                //
-                chabok.addTag('CORDOVA-AGAIN');
-                //
-                chabok.setUserInfo({firstName: 'Hussein'});
-            }, function (e){
-                console.error('Fail to register user = ' + JSON.stringify(e));
-            });
-        }, function (error) {
-            chabok.registerAsGuest(function(s){
-                console.log('------------------ Registered successfully, s = ' + s);
-
-                chabok.track('guestUser', {id: 123});
-                chabok.addTag('CORDOVA');
-                //
-                chabok.setUserInfo({firstName: 'Hussein'});
-            }, function(e){
-                console.error('----------------- Fail to register = ' + e);
-            });
-        })
+        console.log('CHABOK INITIALIZED SUCCESSFULLY');
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
+        console.log('!!!!!!!!!!! DOM is READY !!!!!!!!!!');
+
+        document.getElementById("btnRegister").addEventListener("click", function() {
+            var userId = document.getElementById("txtUserId").value;
+            if (userId !== undefined && userId != null && userId != "") {
+                console.log('######### Login button clicked! #########');
+                chabok.login(userId);
+            }
+        });
+
+        document.getElementById("btnUnregister").addEventListener("click", function() {
+            console.log('######### Logout button clicked! #########');
+            chabok.logout();
+        });
+
+        document.getElementById("btnAddTag").addEventListener("click", function() {
+            var tag = document.getElementById("txtTagName").value;
+            if (tag !== undefined && tag != null && tag != "") {
+                console.log('######### Add Tag button clicked! #########');
+                chabok.addTag(tag);
+            }
+        });
+
+        document.getElementById("btnRemoveTag").addEventListener("click", function() {
+            var tag = document.getElementById("txtTagName").value;
+            if (tag !== undefined && tag != null && tag != "") {
+                console.log('######### Remove Tag button clicked! #########');
+                chabok.removeTag(tag);
+            }
+        });
+
+        document.getElementById("btnAddToCard").addEventListener("click", function() {
+            console.log('######### AddToCart button clicked! #########');
+            chabok.track('AddToCart', {id: 123, seen: true});
+        });
+
+        document.getElementById("btnPurchase").addEventListener("click", function() {
+            console.log('######### Purchase button clicked! #########');
+            chabok.track('Purchase', {id: 123, isRevenue: true});
+        });
+
+        document.getElementById("btnLike").addEventListener("click", function() {
+            console.log('######### Like button clicked! #########');
+            chabok.track('Like', {id: 123, seen: true});
+        });
+
+        document.getElementById("btnComment").addEventListener("click", function() {
+            console.log('######### Comment button clicked! #########');
+            chabok.track('Comment', {id: 123, liked: false});
+        });
+
+        document.getElementById("btnSetUserInfo").addEventListener("click", function() {
+            console.log('######### User Info button clicked! #########');
+            var _firstName = document.getElementById("txtFirstname").value;
+            var _lastName = document.getElementById("txtLastname").value;
+            var _gender = document.getElementById("txtGender").value;
+            if (_firstName !== undefined && _firstName != null && _firstName != ""
+                && _lastName !== undefined && _lastName != null && _lastName != ""
+                && _gender !== undefined && _gender != null && _gender != "") {
+
+                console.log('@@@@@@@@@@ send User Info @@@@@@@@@@');
+                chabok.setUserAttributes({firstName: _firstName, lastName: _lastName, gender: _gender});
+            }
+        });
     }
 };
 
